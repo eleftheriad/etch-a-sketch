c61c4ed22b521420bc0f6aeb1d4d5d16c539e2c0
petros11888 <petroseleftheriadis8@gmail.com>
2022-05-20 14:08:24 +0300

    reset working

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+const container = document.querySelector(".container")
 create_game();
 
 
@@ -8,34 +8,49 @@ function create_game(){
         alert("please enter a number up to 100");
         num_of_blocks = prompt("insert number of blocks");
     }
-    const container = document.querySelector(".container")
-    var A = new Array(num_of_blocks);
+    
     container.style.cssText += "grid-template-columns: repeat(" + num_of_blocks +",1fr)";
     container.style.cssText += "grid-template-rows: repeat(" + num_of_blocks +",1fr)";
     for (let i = 0; i < Math.pow(num_of_blocks,2); i++) {
-        A[i] = document.createElement("div");
-        A[i].classList.add("blk")
-        container.appendChild(A[i]);
+        var div = document.createElement("div");
+        div.classList.add("blk");
+        container.appendChild(div);
+        console.log('hi');
     }
+    
+    
+    
 };
 
+let blocks = document.querySelectorAll('.blk');
+    blocks.forEach((block) => {
+        block.addEventListener('mouseover', () => {
+            block.classList.add("hovered");
+        });
+    });
 
-const blocks = document.querySelectorAll('.blk');
-blocks.forEach((block) => {
-    block.addEventListener('mouseover', () => {
-        block.classList.add("hovered");
-    })
-});
 
 const reset = document.querySelector(".btn-1");
 reset.addEventListener('click', function() {
+    let blocks = document.querySelectorAll('.blk');
     blocks.forEach((block) => {
-            block.classList.remove("hovered");
+        block.classList.remove("hovered");
+    });
+    blocks.forEach((block) => {
+        block.remove();
     });
     reset_game();
 });
 
 
 function reset_game(){
+    container.style.removeProperty("grid-template-columns");
+    container.style.removeProperty("grid-template-rows");
     create_game();
+    let blocks = document.querySelectorAll('.blk');
+    blocks.forEach((block) => {
+        block.addEventListener('mouseover', () => {
+            block.classList.add("hovered");
+        });
+    });
 };
